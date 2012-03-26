b89ec6a5e803483714d7ac1e313be541de5f1490
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-03-27 01:27:37 +0300

    major ui improvements, offer page works, navbar restyle

---
+++ app/helpers/offer_helper.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+module OfferHelper
+  def discount(price, initial_price)
+    '25%' # work it out
+  end
+
+  def win(price, initial_price)
+    (initial_price - price).to_s.gsub('.', ',') + '€'
+  end
+
+  def format_price(price)
+    price = price.to_s
+    if price =~ /\./
+      raw price.gsub('.', '<span class="smaller">,') + '€</span>' # => take this to database level (localization)
+    else
+      raw price + '<span class="smaller>€</span>'
+    end
+  end
+end
