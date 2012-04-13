e823f9091b7065276fa4bde43e8f812c5c8479d0
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-04-13 05:22:39 +0300

    offer page also sets the vendor session

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -5,8 +5,9 @@ class OffersController < ApplicationController
   def show
     @offer = Offer.find(params[:id])
     @vendor = @offer.vendor
-    @cities = @vendor.cities
-
+    # @cities = @vendor.cities
     @more_from_vendor = @vendor.offers.where("id != ?", params[:id]).limit(8)
+
+    session[:vendor_id] = @vendor.id
   end
 end
