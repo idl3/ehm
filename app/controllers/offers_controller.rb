6622330b77fecaabaca349c3524af6d06b5385aa
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-04-19 19:13:30 +0300

    some polish

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -6,9 +6,7 @@ class OffersController < ApplicationController
   def show
     @offer = Offer.find(params[:id])
     @vendor = @offer.vendor
-    # @cities = @vendor.cities
     @more_from_vendor = @vendor.offers.active.where("id != ?", params[:id]).limit(8)
-
     session[:vendor_id] = @vendor.id
   end
 end
