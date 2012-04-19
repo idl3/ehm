b74f5da306192f0453958505bcf560ff36f76b0e
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-04-19 21:46:39 +0300

    refactor categories menu. use only one shared partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,30 +1,31 @@
 # encoding: utf-8
 
 module ApplicationHelper
-  def vendor_categories
-    Vendor.find(params[:id]).categories.all
-  end
-
-  def city_categories
-    current_city.categories
+  def categories
+    case controller_name
+    when 'vendors'
+      Vendor.find(params[:id]).categories
+    when 'cities'
+      City.find(params[:id]).categories
+    end
   end
 
   def title(page_title)
     content_for(:title) { page_title }
   end
 
-  def breadcrumbs
-    case controller_name
-    when 'vendors'
-      heading = current_city.name + " › " + @vendor.name
-      params[:c].nil? ? heading : heading + " › " + active_category.title
-    when 'cities'
-      heading = @city.name + " › " + "Όλα τα Supermarkets"
-      params[:c].nil? ? heading : heading + " › " + active_category.title
-    end
-  end
-
   def active_category
     Category.find(params[:c])
   end
+
+  # def breadcrumbs
+  #   case controller_name
+  #   when 'vendors'
+  #     heading = current_city.name + " › " + @vendor.name
+  #     params[:c].nil? ? heading : heading + " › " + active_category.title
+  #   when 'cities'
+  #     heading = @city.name + " › " + "Όλα τα Supermarkets"
+  #     params[:c].nil? ? heading : heading + " › " + active_category.title
+  #   end
+  # end
 end
