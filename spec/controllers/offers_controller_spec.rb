7046300e2b8adacc6c5c95d422be60582fab6eec
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-04-21 23:22:49 +0300

    enable the spec & implement a simple test

--- spec/controllers/offers_controller_spec.rb
+++ spec/controllers/offers_controller_spec.rb
@@ -1,17 +1,16 @@
-# # encoding: utf-8
-# require 'spec_helper'
+# encoding: utf-8
+require 'spec_helper'
 
-# describe OffersController do
-#   let!(:offer) { FactoryGirl.create(:offer) }
-#   let!(:city)   { FactoryGirl.create(:city) }
+describe OffersController do
+  let!(:offer) { FactoryGirl.create(:offer) }
+  let!(:city)   { FactoryGirl.create(:city) }
 
+  describe "GET show" do
+    it "assigns the city" do
+      cookies[:city_id] = city.id
+      get :show, id: offer.id
+      assigns(:offer).should == offer
+    end
+  end
 
-#   describe "GET show" do
-#     it "assigns the city" do
-#       cookies[:city_id] = city.id
-#       get :show
-#       assigns(:offer).should eq([offer])
-#     end
-#   end
-
-# end
+end
