9fca538954fb4131147f40f226f19e4ee111f0b4
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-04-21 23:38:08 +0300

    test day! add City model specs

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,8 @@
 class City < ActiveRecord::Base
   attr_accessible :name
 
+  validates :name, presence: true
+
   has_and_belongs_to_many :vendors, uniq: true
   has_many :offers, through: :vendors
   has_many :categories, through: :offers, uniq: true
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe City do
+  before { @city = City.new(name: "City") }
 
+  subject { @city }
+
+  it { should be_valid }
+  it { should respond_to :name }
+
+  describe "with blank title" do
+    before { @city.name = " " }
+    it { should_not be_valid }
+  end
 end
