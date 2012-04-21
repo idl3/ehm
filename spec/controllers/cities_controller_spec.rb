5cd0218fc3988123e97b2b0af35b1eade80a24c0
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-04-21 22:54:36 +0300

    use a factory (do it right)

--- spec/controllers/cities_controller_spec.rb
+++ spec/controllers/cities_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe CitiesController do
-
   describe "GET show" do
+    let!(:city)   { FactoryGirl.create(:city) }
 
     describe "with city cookie NOT set" do
       it "redirects to welcome path" do
@@ -13,14 +13,16 @@ describe CitiesController do
     end
 
     describe "with city cookie set" do
-      it "lists all offers from stored city" do
-        cookies[:city_id] = 1
+      it "lists all offers from the selected city" do
+        cookies[:city_id] = city.id
         get :show
         response.should be_success
         response.should render_template 'show'
         assigns(:city).should eq City.find(cookies[:city_id])
       end
     end
+
+    # test for params?
 
   end
 end
